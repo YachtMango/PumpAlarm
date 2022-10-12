ef1a2b45d82d72c4c557e8d218cc71834a543e1c
Nigel Armstrong <yachtmango@gmail.com>
2022-10-12 18:43:54 +0100

    Testing

--- .vscode/PumpControlviaButtons.py
+++ .vscode/PumpControlviaButtons.py
@@ -14,37 +14,42 @@ MESSAGE2 = "Pump running for "
 
 from datetime import datetime
 import sys
-from time import sleep
+import time 
 import threading
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 import automationhat as ahm
 import RPi.GPIO as GPIO
 import ST7735 as ST7735
 from vcgencmd import Vcgencmd
 vcgm = Vcgencmd()
 
-sleep(0.1) # Short pause after ads1015 class creation recommended
+time.sleep(0.1) # Short pause after ads1015 class creation recommended
 
-def LCD():
-    # Create ST7735 LCD display class.
-    disp = ST7735.ST7735(
-        port=0,
-        cs=ST7735.BG_SPI_CS_FRONT,
-        dc=9,
-        backlight=25,
-        rotation=270,
-        spi_speed_hz=4000000
-    )
+
+# Create ST7735 LCD display class.
+disp = ST7735.ST7735(
+    port=0,
+    cs=ST7735.BG_SPI_CS_FRONT,
+    dc=9,
+    backlight=25,
+    rotation=270,
+    spi_speed_hz=4000000
+)
 
 # Initialize display.
 disp.begin()
 
+WIDTH = disp.width
+HEIGHT = disp.height
+
 img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30)
-size_x, size_y = draw.textsize(MESSAGE, font)
+size_x, size_y = draw.textsize(MESSAGE1, font)
 text_x = 160
 text_y = (80 - size_y) // 2
-
 t_start = time.time()
 
 while True:
@@ -53,3 +58,25 @@ while True:
     draw.rectangle((0, 0, 160, 80), (0, 0, 0))
     draw.text((int(text_x - x), text_y), MESSAGE1, font=font, fill=(255, 255, 255))
     disp.display(img)
+    while True:
+        if ahm.input.one.is_on(): 
+            ctemp = vcgm.measure_temp()
+            GPIO.output(25,1) # Ensure backlight is on 
+            ahm.relay.one.on() # when Input 1 is High the run pump for sleep time A
+            x = (time.time() - t_start) * 100
+            x %= (size_x + 160)
+            draw.rectangle((0, 0, 160, 80), (0, 0, 0))
+            draw.text((int(text_x - x), text_y), MESSAGE1, font=font, fill=(255, 255, 255))
+            disp.display(img)
+            with open(LOG_FILE,'a') as l:
+                l.write(datetime.now().strftime("%a %d/%m/%Y, %H:%M") + " Runtime " + str(INPUTA) + " Input Volts " + str(ahm.analog.one.read())  + " CPU Temp = " + str(ctemp) + " °C" "\n")
+            time.sleep(INPUTA)
+            ahm.relay.one.off()
+        elif ahm.input.two.is_on():
+            ctemp = vcgm.measure_temp() 
+            GPIO.output(25,1) # Ensure backlight is on
+            ahm.relay.one.on()  # when Input 2 is High the run pump for sleep time B
+            with open(LOG_FILE,'a') as l:
+                l.write(datetime.now().strftime("%a %d/%m/%Y, %H:%M") + " Runtime " + str(INPUTB) + " Input Volts " + str(ahm.analog.one.read())  + " CPU Temp = " + str(ctemp) + " °C" "\n")
+            time.sleep(INPUTB)
+            ahm.relay.one.off()
