7d3d4d18c0f485642e80353373571b912cd02dfc
Nigel <yachtmango@gmail.com>
2022-11-29 12:52:20 +0000

    Updates

--- Pandawrite.py
+++ Pandawrite.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv("/home/mango3/Downloads/Pump Log File.csv", index_col=0)
+df = pd.read_csv("/home/mango3/Downloads/Pump Log File Epoch DT.csv", index_col=1)
 print(df)
 fig, ax = plt.subplots()
-ax.bar(df["Date"],df["RT"])
+ax.bar(df["Timestamp"],df["RT"])
 plt.show()
--- SQLwrite.py
+++ SQLwrite.py
@@ -1,4 +1,4 @@
-datafileloc = "/home/mango3/Downloads/Pump Log File Clean.csv"
+datafileloc = "/home/mango3/Downloads/Pump Log File Epoch DT.csv"
 dbloc = "/home/mango3/Documents/PumpAlarm/PumpAlarm/PClog.db"
 import sqlite3
 import pandas as pd
@@ -6,7 +6,7 @@ import pandas as pd
 #set up db connection & open cursor
 con = sqlite3.connect(dbloc)
 cur = con.cursor()
-cur.execute("CREATE TABLE pclog(Timestamp timestamp PRIMARY KEY,Day TEXT,Date date ,Time time,ComboDT TEXT,RT INTEGER,V REAL,Temp REAL)")
+cur.execute("CREATE TABLE pclog(Timestamp timestamp PRIMARY KEY,RT INTEGER,V REAL,Temp REAL)")
 
 #load csv into Pandas 
 logdata = pd.read_csv(datafileloc)
