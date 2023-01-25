d1c493e20b2bcf149ac54a2a9919179ec388fbfe
Nigel <yachtmango@gmail.com>
2023-01-25 13:35:55 +0000

    Updates

--- SQLwrite.py
+++ SQLwrite.py
@@ -1,4 +1,4 @@
-datafileloc = "/home/mango3/Downloads/Pump Log File Epoch DT.csv"
+datafileloc = "/home/mango3/Downloads/PumpLogFile.csv"
 dbloc = "/home/mango3/Documents/PumpAlarm/PumpAlarm/PClog.db"
 import sqlite3
 import pandas as pd
@@ -6,7 +6,7 @@ import pandas as pd
 #set up db connection & open cursor
 con = sqlite3.connect(dbloc)
 cur = con.cursor()
-cur.execute("CREATE TABLE pclog(Timestamp timestamp PRIMARY KEY,RT INTEGER,V REAL,Temp REAL)")
+cur.execute("CREATE TABLE pclog(Timestamp timestamp PRIMARY KEY,RT INTEGER,Volts REAL,CTemp REAL,ITemp REAL,ETemp REAL)")
 
 #load csv into Pandas 
 logdata = pd.read_csv(datafileloc)
