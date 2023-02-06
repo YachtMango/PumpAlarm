be378db77c304ab4780f33d831b15ebcb456d275
Nigel Armstrong <yachtmango@gmail.com>
2023-02-06 12:54:10 +0000

    Bug fix on volts

--- Flaskapp/pcws.py
+++ Flaskapp/pcws.py
@@ -30,6 +30,7 @@ def status():
 	return render_template('stats.html',volts=volts,etemp=etemp,ctemp=ctemp,itemp=itemp,tnow=tnow)
 @app.route("/update/", methods=['POST'])
 def update():
+    volts = ahm.analog.one.read()
     ctemp = vcgm.measure_temp()
     etemp = extemp.get_temperature() 
     itemp = intemp.get_temperature()
